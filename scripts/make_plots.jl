28fecf42106466c905f74ff577438416f9ef564d
Thomas Zdyrski <thomas.zdyrski@dartmouth.edu>
2024-12-20 10:50:03 -0500

    Add missing parameter ranges

--- scripts/make_plots.jl
+++ scripts/make_plots.jl
@@ -11,7 +11,7 @@ for num_time_steps in Int.([2E3, 2E6, 2E8])
     for adj_matrix_source in ["well-mixed", "c-elegans", "c-elegans-unweighted",
                               "c-elegans-undirected", "c-elegans-undirected-unweighted",
                               "random-regular-graph", "random-regular-digraph"]
-        for selection_strength in [0.005, 0.2]
+        for selection_strength in [0.005, 0.2, 5]
             for symmetry_breaking in [0, 1 / 4, 1 / 2, 3 / 4, 1]
                 local_moran_interaction.plot_cumulative(selection_strength,
                                                         symmetry_breaking,
@@ -26,7 +26,7 @@ end
 for adj_matrix_source in ["well-mixed", "c-elegans", "c-elegans-unweighted",
                           "c-elegans-undirected", "c-elegans-undirected-unweighted",
                           "random-regular-graph", "random-regular-digraph"]
-    for (B_factor, selection_strength) in zip([1.5, 2.5], [0.2, 5])
+    for (B_factor, selection_strength) in zip([0.005, 1.5, 2.5], [0.2, 5])
         for num_time_steps in Int.([8E4, 8E5, 8E6])
             for symmetry_breaking in [0, 1 / 4, 1 / 2, 3 / 4, 1]
                 local_moran_interaction.plot_timeseries(B_factor, selection_strength,

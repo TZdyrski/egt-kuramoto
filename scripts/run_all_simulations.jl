80dde4d894a68033914dccf1a3fbd9cd36cf61f5
Thomas Zdyrski <thomas.zdyrski@dartmouth.edu>
2025-09-02 09:22:20 -0400

    Remove extraneous num_time_step simulations

--- scripts/run_all_simulations.jl
+++ scripts/run_all_simulations.jl
@@ -7,33 +7,31 @@ include(srcdir("julia", "run_simulations.jl"))
 update_method = "single-update"
 
 # Calc cumulative
-for num_time_steps in Int.([2E3, 2E6, 2E8])
-    for adj_matrix_source in ["well-mixed", "c-elegans", "c-elegans-unweighted",
-                              "c-elegans-undirected", "c-elegans-undirected-unweighted",
-                              "random-regular-graph", "random-regular-digraph"]
-        for selection_strength in [0.005, 0.2, 5]
-            for symmetry_breaking in [0, 1 / 4, 1 / 2, 3 / 4, 1]
-                calc_and_save_cumulative(selection_strength,
-                                                        symmetry_breaking,
-                                                        adj_matrix_source, update_method,
-                                                        num_time_steps)
-            end
-        end
-    end
+num_time_steps_cumulative = Int(2E8)
+for adj_matrix_source in ["well-mixed", "c-elegans", "c-elegans-unweighted",
+		      "c-elegans-undirected", "c-elegans-undirected-unweighted",
+		      "random-regular-graph", "random-regular-digraph"]
+    for selection_strength in [0.005, 0.2, 5]
+        for symmetry_breaking in [0, 1 / 4, 1 / 2, 3 / 4, 1]
+		calc_and_save_cumulative(selection_strength,
+							symmetry_breaking,
+							adj_matrix_source, update_method,
+							num_time_steps_cumulative)
+	    end
+	end
 end
 
 # Calc time-series
+num_time_steps_timeseries = Int(8E6)
 for adj_matrix_source in ["well-mixed", "c-elegans", "c-elegans-unweighted",
                           "c-elegans-undirected", "c-elegans-undirected-unweighted",
                           "random-regular-graph", "random-regular-digraph"]
     for (B_to_c, selection_strength) in Iterators.product([1.5, 2.5], [0.005, 0.2, 5])
-        for num_time_steps in Int.([8E4, 8E5, 8E6])
-            for symmetry_breaking in [0, 1 / 4, 1 / 2, 3 / 4, 1]
-                calc_and_save_timeseries(B_to_c, selection_strength,
-                                                        symmetry_breaking,
-                                                        adj_matrix_source, update_method,
-                                                        num_time_steps)
-            end
+        for symmetry_breaking in [0, 1 / 4, 1 / 2, 3 / 4, 1]
+            calc_and_save_timeseries(B_to_c, selection_strength,
+                                                    symmetry_breaking,
+                                                    adj_matrix_source, update_method,
+                                                    num_time_steps_timeseries)
         end
     end
 end
